0ae32308f33271eaf8d85c91642884e15488e630
Ersamey <meiliaersa@gmail.com>
2024-12-27 21:40:44 +0700

    pesan1

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,22 @@
 
 namespace App\Controllers;
 
+use PHPUnit\Event\Telemetry\Duration;
+
 class Home extends BaseController
 {
     public function index(): string
     {
         return view('welcome_message');
+    }
+
+    public function pesan()
+    {
+        $data = [
+            'title' => 'Pesan',
+            'nama' => 'Rizky Khapidsyah',
+            'pesan' => 'Ini adalah pesan dari controller Home'
+        ];
+        return view('pesan', $data);
     }
 }
---
+++ app/Views/pesan.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?> 3</title>
+</head>
+
+<body>
+    <h1>UPGRADING 3</h1>
+    <h5>Nama : <?= $nama ?></h5>
+    <h5>Pesan : <?= $pesan ?></h5>
+
+</body>
+
+</html>
